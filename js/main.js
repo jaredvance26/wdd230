44e908688faabcb7c04437b224ed336508f6e724
jaredvance26 <jaredkvance@gmail.com>
2022-01-08 17:59:38 -0700

    added js for year

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 alert(document.lastModified);
 
 let lastModified = new Date(document.lastModified);
+let today = new Date()
 
-
+document.getElementById("year").innerText = today.getFullYear();
 document.getElementById("date").innerText = lastModified.toLocaleDateString();
 document.getElementById("time").innerText = lastModified.toLocaleTimeString();
