b33c74c432d788e6addbcb5da4b97faccafc883e
jaredvance26 <jaredkvance@gmail.com>
2022-02-12 16:28:31 -0700

    added current page highlited, moved X to right side on open, optimized images, added hover effect with small view

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -3,4 +3,5 @@ menu.onclick = () => {
   document.getElementById("main_nav").classList.toggle("open");
   document.getElementById("ham_btn").classList.toggle("open");
   document.getElementById("menu").classList.toggle("open");
+  
 };
