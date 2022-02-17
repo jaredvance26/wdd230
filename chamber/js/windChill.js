619853c2008708ccaa5303e5f9a1e9487e70a432
jaredvance26 <jaredkvance@gmail.com>
2022-02-16 19:48:52 -0700

    completed lesson 6 lazy loading and fixed wind chill so that it will be only calculated if the temp is above 50 and speed above 3

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -3,7 +3,12 @@ const temp = Number(document.getElementById("temp").textContent);
 const speed = Number(document.getElementById("speed").textContent);
 
 function chillFactor(t,s) {
+    if(t <=50 & s >= 3) {
     return Math.round(35.74 + (0.6215 *t) - (35.75 * Math.pow(s,.16)) + (0.4275 * t * Math.pow(s,.16)));
+    }
+    else {
+        return("N/A")
+    }
 }
 
 document.getElementById("chill").textContent = chillFactor(temp,speed);
