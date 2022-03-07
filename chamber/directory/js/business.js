0011058b1bb78596b94c7eb2cd90353c8e96886d
jaredvance26 <jaredkvance@gmail.com>
2022-03-07 12:47:47 -0700

    added basic functionality to directory page.

--- chamber/directory/js/business.js
+++ chamber/directory/js/business.js
@@ -1,5 +1,7 @@
 const json_file = "https://raw.githubusercontent.com/jaredvance26/wdd230/main/chamber/directory/js/data.json";
-let cardView = document.querySelector("div.class")
+const cardView = document.querySelector("div.cards");
+const listView = document.querySelector("div.table");
+let table = document.createElement("table");
 
 fetch(json_file)
     .then(response => response.json())
@@ -7,9 +9,8 @@ fetch(json_file)
 
         for (let company of data.companies) {
             createCard(company);
-
-
-        };
+            createTable(company);
+        }
 
         function createCard(company) {
             let card = document.createElement("div");
@@ -20,25 +21,48 @@ fetch(json_file)
             img.alt = "pic of logo"
 
             let address = document.createElement("p");
-            address.innerText = company.address;
+            address.textContent = company.address;
 
             let number = document.createElement("p");
-            number.innerText = company.phone;
+            number.textContent = company.phone;
 
-            let website = document.createElement("p");
-            website.innerText = company.website;
+            let website = document.createElement("a");
+            website.textContent = company.website;
+            website.href= company.website;
 
             card.appendChild(img);
             card.appendChild(address);
             card.appendChild(number);
             card.appendChild(website);
 
+            cardView.appendChild(card);
+          };
+        
+
+        function createTable(company) {
+            let row = document.createElement("tr");
+
+            let compName = document.createElement("td");
+            compName.appendChild(document.createTextNode(company.name));
+
+            let address = document.createElement("td");
+            address.appendChild(document.createTextNode(company.address));
+
+            let number = document.createElement("td");
+            number.appendChild(document.createTextNode(company.phone));
+
+            row.appendChild(compName);
+            row.appendChild(address);
+            row.appendChild(number);
             
+            table.appendChild(row);
+
+            listView.appendChild(table)
 
             
-            
 
 
-        }
+        };
+          
         
     })
---
+++ chamber/directory/js/button.js
@@ -0,0 +1,14 @@
+const card = document.getElementById("card");
+const list = document.getElementById("list");
+
+
+
+
+list.onclick = () => {
+    document.getElementById("switch").classList.remove('card')
+    document.getElementById("switch").classList.toggle("list");
+};
+card.onclick = () => {
+    document.getElementById("switch").classList.remove('list')
+    document.getElementById("switch").classList.toggle("card");
+}
