14a863d05ef12411070ebf2b546fdb7eca3492b5
jaredvance26 <jaredkvance@gmail.com>
2022-03-06 17:57:07 -0700

    fixed error in json file

--- chamber/directory/js/business.js
+++ chamber/directory/js/business.js
@@ -1,6 +1,6 @@
 json_file = "https://raw.githubusercontent.com/jaredvance26/wdd230/main/chamber/directory/js/data.json";
 fetch(json_file)
-.then(response => response.json())
+    .then(response => response.json())
     .then(data => {
-        console.log(data.companies[1]);
+        console.log(data);
     })
