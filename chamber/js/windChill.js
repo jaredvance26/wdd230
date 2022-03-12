b57ad709051ec63015fcd04c5dfbdcb910c0f246
jaredvance26 <jaredkvance@gmail.com>
2022-03-12 10:51:33 -0700

    added weather api to get current temp, wind, desc, and an icon

--- chamber/js/windChill.js
+++ chamber/js/windChill.js
@@ -1,14 +1,45 @@
+url =
+  "https://api.openweathermap.org/data/2.5/weather?id=5310193&appid=34dd0d127d4ea30eac4da8671ffa60e3&units=imperial";
 
-const temp = Number(document.getElementById("temp").textContent);
-const speed = Number(document.getElementById("speed").textContent);
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    
 
-function chillFactor(t,s) {
-    if(t <=50 & s >= 3) {
-    return Math.round(35.74 + (0.6215 *t) - (35.75 * Math.pow(s,.16)) + (0.4275 * t * Math.pow(s,.16)));
+    const temp = Math.round(data.main.temp);
+    const wind = Math.round(data.wind.speed);
+
+    const icon = data.weather[0].icon;
+    const desc = data.weather[0].description;
+
+    src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+    document.getElementById("icon").src = src;
+    document.getElementById("icon").alt = `${desc} icon`;
+
+    document.getElementById("desc").textContent = desc;
+
+    document.getElementById("temp").textContent = temp;
+    document.getElementById("speed").textContent = wind;
+
+    const t = Number(document.getElementById("temp").textContent);
+    const s = Number(document.getElementById("speed").textContent);
+
+
+    document.getElementById("chill").textContent = chillFactor(t, s);
+
+    function chillFactor(t, s) {
+      if ((t <= 50) & (s >= 3)) {
+        return Math.round(
+          35.74 +
+            0.6215 * t -
+            35.75 * Math.pow(s, 0.16) +
+            0.4275 * t * Math.pow(s, 0.16)
+        );
+      } else {
+        return "N/A";
+      }
     }
-    else {
-        return("N/A")
-    }
-}
+  });
 
-document.getElementById("chill").textContent = chillFactor(temp,speed);
+
