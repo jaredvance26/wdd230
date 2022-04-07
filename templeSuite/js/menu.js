6fd65185d59950650d15a1a428ec1ce9255ae383
jaredvance26 <jaredkvance@gmail.com>
2022-04-06 22:29:09 -0600

    fix

--- templeSuite/js/menu.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById("button");
-
-button.addEventListener("click", () => {
-    document.querySelector("header.menu").classList.toggle("switch")
-    document.querySelector("main.blur").classList.toggle("switch")
-   
-})
